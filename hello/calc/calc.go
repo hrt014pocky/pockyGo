7edee215dd7ea39d56ac97a038642cc27ea8ec92
hrt014 <alz1111@163.com>
2022-01-28 01:37:06 +0800

    clone and push test

---
+++ hello/calc/calc.go
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2022-01-28 01:13:22
+ * @LastEditors: Szang
+ * @LastEditTime: 2022-01-28 01:14:36
+ * @FilePath: \myGo\hello\calc\calc.go
+ */
+package calc
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("hello")
+}
---
+++ hello/main.go
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2022-01-28 01:13:07
+ * @LastEditors: Szang
+ * @LastEditTime: 2022-01-28 01:15:01
+ * @FilePath: \myGo\hello\main.go
+ */
+package main
+
+import "hello/calc"
+
+func main() {
+	calc.SayHello()
+}
